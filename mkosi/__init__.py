fe30ff39af93aecff2a6dbe48dd5497971cebe3b
Lennart Poettering <lennart@poettering.net>
2021-03-18 16:54:11 +0100

    mkosi: insert generated file systems sparsely
    Let's lower our disk footprint a bit, by copying in file systems
    sparsely.

--- mkosi/__init__.py
+++ mkosi/__init__.py
@@ -3743,7 +3743,7 @@ def insert_partition(
 
     path = dev if dev is not None else partition(loopdev, partno)
     try:
-        run(["dd", f"if={blob.name}", f"of={path}", "conv=nocreat"])
+        run(["dd", f"if={blob.name}", f"of={path}", "conv=nocreat,sparse"])
     finally:
         luks_close(dev, "Closing LUKS root partition")
 
