4663f8d76ea06b9cbfc66de7a07a88429b8d135a
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-05-21 13:51:51 +0200

    mkosi: rewrite os-release parsing
    This used the code from https://github.com/systemd/systemd/pull/19686,
    except that walrus is removed for python3.7 compatibility.

--- mkosi/__init__.py
+++ mkosi/__init__.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: LGPL-2.1+
 
 import argparse
+import ast
 import collections
 import configparser
 import contextlib
@@ -13,6 +14,7 @@ import datetime
 import enum
 import errno
 import fcntl
+import functools
 import getpass
 import glob
 import hashlib
@@ -166,6 +168,36 @@ arg_debug = ()
 
 class MkosiException(Exception):
     """Leads to sys.exit"""
+
+
+def dictify(f: Callable[..., Generator[Tuple[str, str], None, None]]) -> Callable[..., Dict[str, str]]:
+    def wrapper(*args: Any, **kwargs: Any) -> Dict[str, str]:
+        return dict(f(*args, **kwargs))
+
+    return functools.update_wrapper(wrapper, f)
+
+
+@dictify
+def read_os_release() -> Generator[Tuple[str, str], None, None]:
+    try:
+        filename = "/etc/os-release"
+        f = open(filename)
+    except FileNotFoundError:
+        filename = "/usr/lib/os-release"
+        f = open(filename)
+
+    for line_number, line in enumerate(f, start=1):
+        line = line.rstrip()
+        if not line or line.startswith("#"):
+            continue
+        m = re.match(r"([A-Z][A-Z_0-9]+)=(.*)", line)
+        if m:
+            name, val = m.groups()
+            if val and val[0] in "\"'":
+                val = ast.literal_eval(val)
+            yield name, val
+        else:
+            print(f"{filename}:{line_number}: bad line {line!r}", file=sys.stderr)
 
 
 def print_running_cmd(cmdline: Iterable[str]) -> None:
@@ -5314,55 +5346,35 @@ def parse_bytes(num_bytes: Optional[str]) -> Optional[int]:
 
 def detect_distribution() -> Tuple[Optional[Distribution], Optional[str]]:
     try:
-        f = open("/etc/os-release")
-    except IOError:
-        try:
-            f = open("/usr/lib/os-release")
-        except IOError:
-            return None, None
+        os_release = read_os_release()
+    except FileNotFoundError:
+        return None, None
 
-    dist_id = None
-    version_id = None
-    version_codename = None
+    dist_id = os_release.get("ID", "linux")
+    dist_id_like = os_release.get("ID_LIKE", "").split()
+    version = os_release.get("VERSION", None)
+    version_id = os_release.get("VERSION_ID", None)
+    version_codename = os_release.get("VERSION_CODENAME", None)
     extracted_codename = None
-    dist_id_like = []
 
-    for ln in f:
-        if ln.startswith("ID="):
-            dist_id = ln[3:].strip(" \t\n\"'")
-        if ln.startswith("ID_LIKE="):
-            dist_id_like = ln[8:].strip(" \t\n\"'").split()
-        if ln.startswith("VERSION_ID="):
-            version_id = ln[11:].strip(" \t\n\"'")
-        if ln.startswith("VERSION_CODENAME="):
-            version_codename = ln[17:].strip(" \t\n\"'")
-        if ln.startswith("VERSION="):
-            # extract Debian release codename
-            version_str = ln[8:].strip(" \t\n\"'")
-            debian_codename_re = r"\((.*?)\)"
-
-            codename_list = re.findall(debian_codename_re, version_str)
-            if len(codename_list) == 1:
-                extracted_codename = codename_list[0]
+    if version:
+        # extract Debian release codename
+        m = re.search(r"\((.*?)\)", version)
+        if m:
+            extracted_codename = m.group(1)
 
     if dist_id == "clear-linux-os":
         dist_id = "clear"
 
     d: Optional[Distribution] = None
-    if dist_id is not None:
-        d = Distribution.__members__.get(dist_id, None)
-        if d is None:
-            for dist_id in dist_id_like:
-                d = Distribution.__members__.get(dist_id, None)
-                if d is not None:
-                    break
+    for the_id in [dist_id, *dist_id_like]:
+        d = Distribution.__members__.get(the_id, None)
+        if d is not None:
+            break
 
-    if (d == Distribution.debian or d == Distribution.ubuntu) and (version_codename or extracted_codename):
+    if d in {Distribution.debian, Distribution.ubuntu} and (version_codename or extracted_codename):
         # debootstrap needs release codenames, not version numbers
-        if version_codename:
-            version_id = version_codename
-        else:
-            version_id = extracted_codename
+        version_id = version_codename or extracted_codename
 
     return d, version_id
 
