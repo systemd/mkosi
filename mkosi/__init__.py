286c6ed13d0eef6004d5e19eebf0ab6dc439f8b1
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-09-20 18:42:30 +0200

    Fix indentation in output
    Follow-up for 95aaafb11d.

--- mkosi/__init__.py
+++ mkosi/__init__.py
@@ -6130,7 +6130,7 @@ def print_summary(args: CommandLineArguments) -> None:
         MkosiPrinter.info("                    Mirror: " + args.mirror)
     if args.repositories is not None and len(args.repositories) > 0:
         MkosiPrinter.info("              Repositories: " + ",".join(args.repositories))
-    MkosiPrinter.info("   Use Host Repositories: " + yes_no(args.use_host_repositories))
+    MkosiPrinter.info("     Use Host Repositories: " + yes_no(args.use_host_repositories))
     MkosiPrinter.info("\nOUTPUT:")
     if args.hostname:
         MkosiPrinter.info("                  Hostname: " + args.hostname)
