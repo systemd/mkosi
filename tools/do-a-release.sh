b8ca4db1b761676f345baa182a20b7a853b4c15a
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-11-24 16:06:53 +0100

    tools/do-a-release: rebuild the man page in a separate commit
    Otherwise the interesting changes are hidden behind a very long diff of
    generated output.

--- tools/do-a-release.sh
+++ tools/do-a-release.sh
@@ -6,13 +6,22 @@ if [ -z "$1" ] ; then
     exit 1
 fi
 
+if ! git diff-index --quiet HEAD; then
+    echo "Repo has modified files."
+    exit 1
+fi
+
+pandoc -t man -s -o man/mkosi.1 mkosi.md
+
+if ! git diff-index --quiet HEAD; then
+    git add man/mkosi.1
+    git commit -m "man: rebuild the man page"
+fi
+
 sed -i 's/version=".*",/version="'"$1"'",/' setup.py
 sed -i "s/__version__ = \".*\"/__version__ = \"$1\"/" mkosi/__init__.py
 
 git add -p setup.py mkosi action.yaml
-
-pandoc -t man -s -o man/mkosi.1 mkosi.md
-git add man/mkosi.1
 
 git commit -m "Bump version numbers for v$1"
 
