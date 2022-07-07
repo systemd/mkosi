19364a7c201c83ef3dd275c23e9a27df8cc787c5
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-07-07 20:11:14 +0200

    Explicitly configure kernel-install layout as "bls"

--- mkosi/__init__.py
+++ mkosi/__init__.py
@@ -1847,6 +1847,7 @@ def prepare_tree(args: MkosiArgs, root: Path, do_run_build_script: bool, cached:
 
             root.joinpath("etc/kernel/cmdline").write_text(" ".join(args.kernel_command_line) + "\n")
             root.joinpath("etc/kernel/entry-token").write_text(f"{args.machine_id}\n")
+            root.joinpath("etc/kernel/install.conf").write_text("layout=bls\n")
 
         if do_run_build_script or args.ssh:
             root_home(args, root).mkdir(mode=0o750)
