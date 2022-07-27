846465a0b45472acbfb7b098267cec3ff8a8bf05
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-07-27 20:02:16 +0200

    Only check whether a format can be booted if verb == qemu
    We can still build these images without any problems, we just can't
    boot them, so let's still allow building the images, just not booting
    them.

--- mkosi/__init__.py
+++ mkosi/__init__.py
@@ -6388,7 +6388,7 @@ def load_args(args: argparse.Namespace) -> MkosiArgs:
             args.release = "rolling"
 
     if args.bootable:
-        if args.output_format in (
+        if args.verb == Verb.qemu and args.output_format in (
             OutputFormat.directory,
             OutputFormat.subvolume,
             OutputFormat.tar,
