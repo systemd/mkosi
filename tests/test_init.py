621ed7a818f30dc097d73e34eeb877991f59675d
Pyfisch <pyfisch@posteo.org>
2022-09-11 14:25:14 +0200

    No longer mention black and fix indentation
    Mkosi uses 4 spaces for indentation, fix 2 functions with 8 spaces.
    Since #779 formatting with black wasn't enforced but still mentioned as required in README.

--- tests/test_init.py
+++ tests/test_init.py
@@ -32,29 +32,29 @@ def test_strip_suffixes() -> None:
 
 
 def test_copy_file(tmpdir: Path) -> None:
-        dir_path = Path(tmpdir)
-        file_1 = Path(dir_path) / "file_1.txt"
-        file_2 = Path(dir_path) / "file_2.txt"
-        file_1.touch()
-        file_2.touch()
+    dir_path = Path(tmpdir)
+    file_1 = Path(dir_path) / "file_1.txt"
+    file_2 = Path(dir_path) / "file_2.txt"
+    file_1.touch()
+    file_2.touch()
 
-        # Copying two empty files.
-        mkosi.copy_file(file_1, file_2)
-        assert filecmp.cmp(file_1, file_2)
+    # Copying two empty files.
+    mkosi.copy_file(file_1, file_2)
+    assert filecmp.cmp(file_1, file_2)
 
-        # Copying content from one file.
-        file_1.write_text("Testing copying content from this file to file_2.")
-        mkosi.copy_file(file_1, file_2)
-        assert filecmp.cmp(file_1, file_2)
+    # Copying content from one file.
+    file_1.write_text("Testing copying content from this file to file_2.")
+    mkosi.copy_file(file_1, file_2)
+    assert filecmp.cmp(file_1, file_2)
 
-        # Giving a non existing path/file.
-        with pytest.raises(OSError):
-            mkosi.copy_file("nullFilePath", file_1)
+    # Giving a non existing path/file.
+    with pytest.raises(OSError):
+        mkosi.copy_file("nullFilePath", file_1)
 
-        # Copying when there's already content in both files.
-        file_2.write_text("Testing copying content from file_1 to file_2, with previous data.")
-        mkosi.copy_file(file_1, file_2)
-        assert filecmp.cmp(file_1, file_2)
+    # Copying when there's already content in both files.
+    file_2.write_text("Testing copying content from file_1 to file_2, with previous data.")
+    mkosi.copy_file(file_1, file_2)
+    assert filecmp.cmp(file_1, file_2)
 
 
 def test_parse_bytes() -> None:
