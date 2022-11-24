38b7379764878b758a1edf9d4a3269ec8de22d58
Joerg Behrmann <behrmann@physik.fu-berlin.de>
2022-11-24 16:57:56 +0100

    Split Alma and Rocky out of Centos

--- mkosi/distributions/alma.py
+++ mkosi/distributions/alma.py
@@ -1,7 +1,24 @@
 # SPDX-License-Identifier: LGPL-2.1+
 
+from pathlib import Path
+from typing import Tuple
+
+from mkosi.backend import MkosiConfig
 from mkosi.distributions.centos import CentosInstaller
 
 
 class AlmaInstaller(CentosInstaller):
-    pass
+    @staticmethod
+    def _gpg_locations(epel_release: int) -> Tuple[Path, str]:
+        return (
+            Path("/etc/pki/rpm-gpg/RPM-GPG-KEY-AlmaLinux"),
+            "https://repo.almalinux.org/almalinux/RPM-GPG-KEY-AlmaLinux"
+        )
+
+    @classmethod
+    def _mirror_directory(cls) -> str:
+        return "almalinux"
+
+    @classmethod
+    def _mirror_repo_url(cls, config: MkosiConfig, repo: str) -> str:
+        return f"https://mirrors.almalinux.org/mirrorlist/{config.release}/{repo.lower()}"
