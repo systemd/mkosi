0bbb31eed90d8e5e6685e38a8e7b10fa4493c1b3
Luca Boccassi <bluca@debian.org>
2023-02-23 13:07:10 +0000

    ubuntu: security repo is different from !x86

--- mkosi/distributions/ubuntu.py
+++ mkosi/distributions/ubuntu.py
@@ -22,8 +22,11 @@ class UbuntuInstaller(DebianInstaller):
         updates = f"deb {state.config.mirror} {state.config.release}-updates {' '.join(repos)}"
         state.root.joinpath(f"etc/apt/sources.list.d/{state.config.release}-updates.list").write_text(f"{updates}\n")
 
-        # Security updates repos are never mirrored
-        security = f"deb http://security.ubuntu.com/ubuntu/ {state.config.release}-security {' '.join(repos)}"
+        # Security updates repos are never mirrored. But !x86 are on the ports server.
+        if state.config.architecture in ["x86", "x86_64"]:
+            security = f"deb http://security.ubuntu.com/ubuntu/ {state.config.release}-security {' '.join(repos)}"
+        else:
+            security = f"deb http://ports.ubuntu.com/ {state.config.release}-security {' '.join(repos)}"
 
         state.root.joinpath(f"etc/apt/sources.list.d/{state.config.release}-security.list").write_text(f"{security}\n")
 
