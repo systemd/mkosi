b0e67ca730b64643e62663ce4cca6f98537b6345
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-04-18 21:48:10 +0200

    Don't make output an absolute path while parsing
    Fixes a regression from 2a6aa720f59e4f66ba63d311f5fa79c3a3ac0a61.

--- mkosi/__init__.py
+++ mkosi/__init__.py
@@ -1186,7 +1186,8 @@ def load_args(args: argparse.Namespace) -> MkosiConfig:
             args.output = args.output_dir / args.output
         else:
             warn("Ignoring configured output directory as output file is a qualified path.")
-            args.output = args.output.absolute()
+
+    args.output = args.output.absolute()
 
     if args.environment:
         env = {}
