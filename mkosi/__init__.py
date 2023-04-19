systemd/mkosi

commit 2a6aa720f59e4f66ba63d311f5fa79c3a3ac0a61

Use absolute paths everywhere
bwrap seems to run into permission errors with relative paths in
some cases so let's follow systemd best practices and convert all
config paths to absolute paths as we're parsing them.

Fixes #1430

mkosi/__init__.py
@@ -1130,6 +1130,7 @@ def load_args(args: argparse.Namespace) -> MkosiConfig:
             args.output = args.output_dir / args.output
         else:
             warn("Ignoring configured output directory as output file is a qualified path.")
+            args.output = args.output.absolute()
 
     if args.environment:
         env = {}

mkosi/config.py
@@ -45,9 +45,9 @@ def parse_source_target_paths(value: str) -> tuple[Path, Optional[Path]]:
     src, _, target = value.partition(':')
     if not Path(src).exists():
         die(f"{src} does not exist")
-    if target and not Path(target).absolute():
+    if target and not Path(target).is_absolute():
         die("Target path must be absolute")
-    return Path(src), Path(target) if target else None
+    return Path(src).absolute(), Path(target) if target else None
 
 
 def config_parse_string(dest: str, value: Optional[str], namespace: argparse.Namespace) -> Optional[str]:
@@ -71,7 +71,7 @@ def config_parse_script(dest: str, value: Optional[str], namespace: argparse.Nam
         if not os.access(value, os.X_OK):
             die(f"{value} is not executable")
 
-    return Path(value) if value else None
+    return Path(value).absolute() if value else None
 
 
 def config_parse_boolean(dest: str, value: Optional[str], namespace: argparse.Namespace) -> bool:
@@ -196,7 +196,7 @@ def make_path_parser(required: bool) -> Callable[[str], Path]:
         if required and not Path(value).exists():
             die(f"{value} does not exist")
 
-        return Path(value)
+        return Path(value).absolute()
 
     return parse_path
 
@@ -209,7 +209,7 @@ def config_make_path_parser(required: bool) -> ConfigParseCallback:
         if value and required and not Path(value).exists():
             die(f"{value} does not exist")
 
-        return Path(value) if value else None
+        return Path(value).absolute() if value else None
 
     return config_parse_path
 

commit ec1c62a77dcc47e1074e8b9904b94f1f06e025a3

Don't make output an absolute path while parsing
Fixes a regression from 2a6aa720f59e4f66ba63d311f5fa79c3a3ac0a61.

mkosi/__init__.py
@@ -1179,7 +1179,8 @@ def load_args(args: argparse.Namespace) -> MkosiConfig:
             args.output = args.output_dir / args.output
         else:
             warn("Ignoring configured output directory as output file is a qualified path.")
-            args.output = args.output.absolute()
+
+    args.output = args.output.absolute()
 
     if args.environment:
         env = {}

mkosi/config.py
@@ -203,7 +203,7 @@ def make_path_parser(required: bool) -> Callable[[str], Path]:
     return parse_path
 
 
-def config_make_path_parser(required: bool) -> ConfigParseCallback:
+def config_make_path_parser(required: bool, absolute: bool = True) -> ConfigParseCallback:
     def config_parse_path(dest: str, value: Optional[str], namespace: argparse.Namespace) -> Optional[Path]:
         if dest in namespace:
             return getattr(namespace, dest) # type: ignore
@@ -211,7 +211,10 @@ def config_make_path_parser(required: bool) -> ConfigParseCallback:
         if value and required and not Path(value).exists():
             die(f"{value} does not exist")
 
-        return Path(value).absolute() if value else None
+        if value:
+            return Path(value).absolute() if absolute else Path(value)
+
+        return None
 
     return config_parse_path
 
@@ -368,7 +371,7 @@ class MkosiConfigParser:
         MkosiConfigSetting(
             dest="output",
             section="Output",
-            parse=config_make_path_parser(required=False),
+            parse=config_make_path_parser(required=False, absolute=False),
         ),
         MkosiConfigSetting(
             dest="output_dir",