95aa11da78cc00f378c68a21c651fcc3ee0b36ea
Luca Boccassi <bluca@debian.org>
2023-09-07 18:54:40 +0100

    Release 16

--- mkosi/config.py
+++ mkosi/config.py
@@ -38,7 +38,7 @@ from mkosi.util import (
 )
 from mkosi.versioncomp import GenericVersion
 
-__version__ = "15.1"
+__version__ = "16"
 
 ConfigParseCallback = Callable[[Optional[str], Optional[Any]], Any]
 ConfigMatchCallback = Callable[[str, Any], bool]
