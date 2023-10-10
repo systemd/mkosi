14292ad13c16b399951983cff38cee60463be3f4
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-10-10 11:36:59 +0200

    Pass around MkosiState in centos based distributions
    We need access to the pkgmngr tree to be able to look for GPG keys
    or certificates there, so let's pass around MkosiState everywhere.

--- mkosi/distributions/rocky.py
+++ mkosi/distributions/rocky.py
@@ -2,9 +2,9 @@
 
 from pathlib import Path
 
-from mkosi.config import MkosiConfig
 from mkosi.distributions import centos
 from mkosi.installer.dnf import Repo
+from mkosi.state import MkosiState
 
 
 class Installer(centos.Installer):
@@ -13,22 +13,22 @@ class Installer(centos.Installer):
         return "Rocky Linux"
 
     @staticmethod
-    def gpgurls(config: MkosiConfig) -> tuple[str, ...]:
-        gpgpath = Path(f"/usr/share/distribution-gpg-keys/rocky/RPM-GPG-KEY-Rocky-{config.release}")
+    def gpgurls(state: MkosiState) -> tuple[str, ...]:
+        gpgpath = Path(f"/usr/share/distribution-gpg-keys/rocky/RPM-GPG-KEY-Rocky-{state.config.release}")
         if gpgpath.exists():
             return (f"file://{gpgpath}",)
         else:
             return ("https://download.rockylinux.org/pub/rocky/RPM-GPG-KEY-Rocky-$releasever",)
 
     @classmethod
-    def repository_variants(cls, config: MkosiConfig, repo: str) -> list[Repo]:
-        if config.mirror:
-            url = f"baseurl={config.mirror}/rocky/$releasever/{repo}/$basearch/os"
+    def repository_variants(cls, state: MkosiState, repo: str) -> list[Repo]:
+        if state.config.mirror:
+            url = f"baseurl={state.config.mirror}/rocky/$releasever/{repo}/$basearch/os"
         else:
             url = f"mirrorlist=https://mirrors.rockylinux.org/mirrorlist?arch=$basearch&repo={repo}-$releasever"
 
-        return [Repo(repo, url, cls.gpgurls(config))]
+        return [Repo(repo, url, cls.gpgurls(state))]
 
     @classmethod
-    def sig_repositories(cls, config: MkosiConfig) -> list[Repo]:
+    def sig_repositories(cls, state: MkosiState) -> list[Repo]:
         return []
