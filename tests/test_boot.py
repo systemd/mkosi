b5b9963298900453446ca407d45cdc0085ef1649
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-12-01 11:22:21 +0100

    Move integration tests into python
    Instead of vendor locking ourselves to Github Actions, let's move
    the integration tests into python so we can run them locally and
    on other CI systems.
    
    We opt to use unittest style test cases so that we can have a
    configurable base class that can be used for various integration
    tests. Unfortunately, I haven't found a nice and type safe way to
    make pytest fixtures configurable so we opt for unittest instead.
    
    Note that while we use the subTest() feature of unittest, pytest
    still considers test_boot() a single test because it doesn't support
    this particular feature of unittest. Ideally we switch our test runner
    to something else in the future which does support the subTest() feature.
    
    We always run steps that can run unprivileged without privileges even
    if we're running as root so that tests can be run locally with root
    privileges without ending up with a bunch of files owned by root
    afterwards.

---
+++ tests/test_boot.py
@@ -0,0 +1,53 @@
+# SPDX-License-Identifier: LGPL-2.1+
+
+import os
+import pytest
+
+from mkosi.config import OutputFormat
+from mkosi.distributions import Distribution
+
+from . import Image
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("format", [f for f in OutputFormat if f != OutputFormat.none])
+def test_boot(format: OutputFormat) -> None:
+    with Image(
+        options=[
+            "--kernel-command-line=console=ttyS0",
+            "--kernel-command-line=systemd.unit=mkosi-check-and-shutdown.service",
+            "--kernel-command-line=systemd.log_target=console",
+            "--kernel-command-line=systemd.default_standard_output=journal+console",
+            "--qemu-vsock=yes",
+            "--qemu-mem=4G",
+            "--incremental",
+            "--ephemeral",
+        ],
+    ) as image:
+        if image.distribution == Distribution.rhel_ubi and format in (OutputFormat.esp, OutputFormat.uki):
+            pytest.skip("Cannot build RHEL-UBI images with format 'esp' or 'uki'")
+
+        options = ["--format", str(format)]
+
+        image.build(options=options)
+
+        if format in (OutputFormat.disk, OutputFormat.directory) and os.getuid() == 0:
+            # systemd-resolved is enabled by default in Arch/Debian/Ubuntu (systemd default preset) but fails
+            # to start in a systemd-nspawn container with --private-users so we mask it out here to avoid CI
+            # failures.
+            # FIXME: Remove when Arch/Debian/Ubuntu ship systemd v253
+            args = ["systemd.mask=systemd-resolved.service"] if format == OutputFormat.directory else []
+            image.boot(options=options, args=args)
+
+        if image.distribution == Distribution.rhel_ubi:
+            return
+
+        if format in (OutputFormat.directory, OutputFormat.tar):
+            return
+
+        image.qemu(options=options)
+
+        if format != OutputFormat.disk:
+            return
+
+        image.qemu(options=options + ["--qemu-firmware=bios"])
