9349236b6f91d032f09008af9a629ab9f2fbf1e5
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-12-10 11:20:56 +0100

    arch: Move arch specific configuration out of setup_pacman()
    Let's make setup_pacman() about pacman and not about Arch.

--- mkosi/distributions/arch.py
+++ mkosi/distributions/arch.py
@@ -5,7 +5,7 @@ from collections.abc import Sequence
 from mkosi.architecture import Architecture
 from mkosi.config import ConfigFeature
 from mkosi.distributions import Distribution, DistributionInstaller, PackageType
-from mkosi.installer.pacman import invoke_pacman, setup_pacman
+from mkosi.installer.pacman import PacmanRepository, invoke_pacman, setup_pacman
 from mkosi.log import die
 from mkosi.state import MkosiState
 
@@ -75,7 +75,32 @@ class Installer(DistributionInstaller):
 
     @classmethod
     def setup(cls, state: MkosiState) -> None:
-        setup_pacman(state)
+        if state.config.local_mirror:
+            repos = [PacmanRepository("core", state.config.local_mirror)]
+        else:
+            repos = []
+
+            if state.config.architecture == Architecture.arm64:
+                url = f"{state.config.mirror or 'http://mirror.archlinuxarm.org'}/$arch/$repo"
+            else:
+                url = f"{state.config.mirror or 'https://geo.mirror.pkgbuild.com'}/$repo/os/$arch"
+
+            # Testing repositories have to go before regular ones to to take precedence.
+            for id in (
+                "core-testing",
+                "core-testing-debug",
+                "extra-testing",
+                "extra-testing-debug",
+                "core-debug",
+                "extra-debug",
+            ):
+                if id in state.config.repositories:
+                    repos += [PacmanRepository(id, url)]
+
+            for id in ("core", "extra"):
+                repos += [PacmanRepository(id, url)]
+
+        setup_pacman(state, repos)
 
     @classmethod
     def install(cls, state: MkosiState) -> None:
