5f81e1c9db15be4b5f0bddac71b1d15a0e285c46
Clayton Craft <clayton@craftyguy.net>
2024-03-10 15:31:06 +0000

    Add postmarketOS support
    Co-authored-by: Casey Connolly <kcxt@postmarketos.org>
    Co-authored-by: Septatrix <24257556+Septatrix@users.noreply.github.com>

--- mkosi/__init__.py
+++ mkosi/__init__.py
@@ -1210,6 +1210,7 @@ def install_sandbox_trees(config: Config, dst: Path) -> None:
         "etc/ssl",
         "etc/ca-certificates",
         "etc/pacman.d/gnupg",
+        "etc/apk/keys",
         "etc/alternatives",
     ):
         (dst / d).mkdir(parents=True, exist_ok=True)
