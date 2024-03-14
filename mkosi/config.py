fcae01677c439e8950d9d20bfb97cac3d01492a5
Luca Boccassi <bluca@debian.org>
2024-03-14 11:19:28 +0000

    Release 22

--- mkosi/config.py
+++ mkosi/config.py
@@ -46,7 +46,7 @@ from mkosi.util import (
 )
 from mkosi.versioncomp import GenericVersion
 
-__version__ = "21"
+__version__ = "22"
 
 ConfigParseCallback = Callable[[Optional[str], Optional[Any]], Any]
 ConfigMatchCallback = Callable[[str, Any], bool]
