7a36ebeecbee5464880662d65512c14bec8cb813
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-03-18 11:24:37 +0100

    ci: Use pcid=off with qemu instead of disabling secure boot
    This allows us to boot with secure boot again until the Hyper-V bug
    is fixed that causes KVM to crash.
    
    We also switch back to using 2Gs of ram by default as a second Hyper-V
    bug prevents us from using any more on Github Actions.

--- tests/test_boot.py
+++ tests/test_boot.py
@@ -32,8 +32,6 @@ def test_format(config: Image.Config, format: OutputFormat) -> None:
             "--kernel-command-line=systemd.unit=mkosi-check-and-shutdown.service",
             "--incremental",
             "--ephemeral",
-            # TODO: Drop once https://bugs.launchpad.net/ubuntu/+source/linux/+bug/2038777 is fixed in Github Actions
-            "--qemu-firmware=uefi" if format in (OutputFormat.disk, OutputFormat.uki) else "--qemu-firmware=auto",
         ],
     ) as image:
         if image.config.distribution == Distribution.rhel_ubi and format in (OutputFormat.esp, OutputFormat.uki):
@@ -81,9 +79,7 @@ def test_bootloader(config: Image.Config, bootloader: Bootloader) -> None:
     if config.distribution == Distribution.rhel_ubi:
         return
 
-    # TODO: Use "auto" again instead of "uefi" once https://bugs.launchpad.net/ubuntu/+source/linux/+bug/2038777 is
-    # fixed in Github Actions.
-    firmware = QemuFirmware.linux if bootloader == Bootloader.none else QemuFirmware.uefi
+    firmware = QemuFirmware.linux if bootloader == Bootloader.none else QemuFirmware.auto
 
     with Image(
         config,
