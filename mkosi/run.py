6b46a43a9e3e0567ec3daf1f1dd0127d23d5791d
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-04-15 12:39:57 +0200

    Make check argument apply to whether the command exists
    Let's not fail hard when check=False and the command is not found.
    This will allow us to avoid having to pass the tools tree path around
    in various places.

--- mkosi/run.py
+++ mkosi/run.py
@@ -144,24 +144,27 @@ def run(
         assert stdin is None  # stdin and input cannot be specified together
         stdin = subprocess.PIPE
 
-    with spawn(
-        cmdline,
-        check=check,
-        stdin=stdin,
-        stdout=stdout,
-        stderr=stderr,
-        user=user,
-        group=group,
-        env=env,
-        cwd=cwd,
-        log=log,
-        foreground=foreground,
-        preexec_fn=preexec_fn,
-        success_exit_status=success_exit_status,
-        sandbox=sandbox,
-        innerpid=False,
-    ) as (process, _):
-        out, err = process.communicate(input)
+    try:
+        with spawn(
+            cmdline,
+            check=check,
+            stdin=stdin,
+            stdout=stdout,
+            stderr=stderr,
+            user=user,
+            group=group,
+            env=env,
+            cwd=cwd,
+            log=log,
+            foreground=foreground,
+            preexec_fn=preexec_fn,
+            success_exit_status=success_exit_status,
+            sandbox=sandbox,
+            innerpid=False,
+        ) as (process, _):
+            out, err = process.communicate(input)
+    except FileNotFoundError:
+        return CompletedProcess(cmdline, 1)
 
     return CompletedProcess(cmdline, process.returncode, out, err)
 
@@ -261,7 +264,13 @@ def spawn(
             prefix + ["sh", "-c", f"command -v {cmdline[0]}"],
             stdout=subprocess.DEVNULL,
         ).returncode != 0:
-            die(f"{cmdline[0]} not found.", hint=f"Is {cmdline[0]} installed on the host system?")
+            if check:
+                die(f"{cmdline[0]} not found.", hint=f"Is {cmdline[0]} installed on the host system?")
+
+            # We can't really return anything in this case, so we raise a specific exception that we can catch in
+            # run().
+            logging.debug(f"{cmdline[0]} not found, not running {shlex.join(cmdline)}")
+            raise FileNotFoundError(cmdline[0])
 
         if (
             foreground and
