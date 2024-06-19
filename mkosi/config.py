a3109a4fa06b1e9a8bdedeaed5798be5b6c10e0c
Jörg Behrmann <behrmann@physik.fu-berlin.de>
2024-06-19 18:55:58 +0200

    Show doc format options in help

--- mkosi/config.py
+++ mkosi/config.py
@@ -3307,6 +3307,7 @@ def create_argument_parser(chdir: bool = True) -> argparse.ArgumentParser:
         help="The format to show documentation in",
         default=DocFormat.auto,
         type=DocFormat,
+        choices=list(DocFormat),
     )
     parser.add_argument(
         "--shell-completion",
