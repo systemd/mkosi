systemd/mkosi

commit 0a1e8f26d16ac64040da7d03ef4711e706775f3a

Make diff debug output in have_cache universal

mkosi/__init__.py
@@ -2779,7 +2779,7 @@ def have_cache(config: Config) -> bool:
             logging.info("Cache manifest mismatch, not reusing cached images")
             if ARG_DEBUG.get():
                 run(
-                    ["diff", manifest, "-"],
+                    ["diff", "-u", "manifest", "-"],
                     input=new,
                     check=False,
                     sandbox=config.sandbox(binary="diff", options=["--bind", manifest, manifest]),

commit e631c112d184c9331f343ee016e1611f659441f7

Fix wrongly-quoted string.
Also make the option long-form.

Fixes: 0a1e8f26d16ac64040da7d03ef4711e706775f3a

mkosi/__init__.py
@@ -2779,7 +2779,7 @@ def have_cache(config: Config) -> bool:
             logging.info("Cache manifest mismatch, not reusing cached images")
             if ARG_DEBUG.get():
                 run(
-                    ["diff", "-u", "manifest", "-"],
+                    ["diff", "--unified", manifest, "-"],
                     input=new,
                     check=False,
                     sandbox=config.sandbox(binary="diff", options=["--bind", manifest, manifest]),