dabd4a2fb963ee73f405601e1e8cf5df62e026ed
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-12-17 12:36:59 +0100

    Improve summary output a bit
    Let's skip a few sections for subimages as these will always be identical
    to the main image anyways.

--- mkosi/config.py
+++ mkosi/config.py
@@ -4722,6 +4722,10 @@ def summary(config: Config) -> str:
 
     summary = f"""\
 {bold(f"IMAGE: {config.image or 'default'}")}
+"""
+
+    if not config.image:
+        summary += f"""\
 
     {bold("CONFIG")}:
                            Profiles: {line_join_list(config.profiles)}
@@ -4739,6 +4743,9 @@ def summary(config: Config) -> str:
            Repo Signature/Key check: {yes_no(config.repository_key_check)}
               Fetch Repository Keys: {yes_no(config.repository_key_fetch)}
                        Repositories: {line_join_list(config.repositories)}
+"""
+
+    summary += f"""\
 
     {bold("OUTPUT")}:
                       Output Format: {config.output_format}
@@ -4849,7 +4856,8 @@ def summary(config: Config) -> str:
                             GPG Key: ({"default" if config.key is None else config.key})
 """
 
-    summary += f"""\
+    if not config.image:
+        summary += f"""\
 
     {bold("BUILD CONFIGURATION")}:
                          Tools Tree: {config.tools_tree}
