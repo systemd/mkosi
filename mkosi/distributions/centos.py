4e1017adabd00537e9edff857e7b3d01716add8c
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-01-15 09:49:31 +0100

    centos: Bump default release to 10

--- mkosi/distributions/centos.py
+++ mkosi/distributions/centos.py
@@ -34,7 +34,7 @@ class Installer(DistributionInstaller):
 
     @classmethod
     def default_release(cls) -> str:
-        return "9"
+        return "10"
 
     @classmethod
     def default_tools_tree_distribution(cls) -> Distribution:
