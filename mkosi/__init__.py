6efefe2e3a431d36f3c5faa6c0a5edc9d3adf843
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-01-29 21:37:02 +0100

    Fix condition when removing duplicate files from the overlay

--- mkosi/__init__.py
+++ mkosi/__init__.py
@@ -185,11 +185,14 @@ def mount_base_trees(context: Context) -> Iterator[None]:
             rel = p.relative_to(context.root)
             q = context.workspace / "lower" / rel
 
-            if not q.is_symlink() and q.is_dir():
-                if p.is_symlink() or not p.is_dir():
-                    die(f"/{rel} is a directory in the base tree but not in the overlay")
+            if not q.is_symlink() and not q.exists():
+                continue
+
+            if not p.is_symlink() and p.is_dir():
+                if q.is_symlink() or not q.is_dir():
+                    die(f"/{rel} is a directory in the overlay but not in the base tree")
                 shutil.copystat(q, p)
-            elif q.is_symlink() or q.exists():
+            else:
                 logging.info(f"Removing duplicate path /{rel} from overlay")
                 p.unlink()
 
