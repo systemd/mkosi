42e6797d0e3c058d0a86790f1c8d7530c6a6dfaf
Jörg Behrmann <behrmann@physik.fu-berlin.de>
2025-02-03 12:57:05 +0100

    completion: complete paths after verb for zsh

--- mkosi/resources/completion.zsh
+++ mkosi/resources/completion.zsh
@@ -7,15 +7,8 @@ _mkosi_verb(){
         _describe -t commands 'mkosi verb' _mkosi_verbs
     else
         local curcontext="$curcontext"
-        cmd="${${_mkosi_verbs[(r)$words[1]:*]%%:*}}"
-        if (( $#cmd )); then
-            if (( $+functions[_mkosi_$cmd] )); then
-                _mkosi_$cmd
-            else
-                _message "no more options"
-            fi
-        else
-            _message "unknown mkosi verb: $words[1]"
+        if [[ "$curcontext" == ':complete:mkosi:argument-rest' ]]; then
+            _files
         fi
     fi
 }
