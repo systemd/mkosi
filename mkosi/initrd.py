684bb3c999012cd04c66a46a3aede5fcf746fb14
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2025-02-28 12:50:23 +0100

    mkosi,addon: print the mkosi cmdline with --debug
    This is very important, without that it's hard to figure out what is actually
    hapenning.

--- mkosi/initrd.py
+++ mkosi/initrd.py
@@ -15,7 +15,7 @@ from typing import Optional, cast
 import mkosi.resources
 from mkosi.config import DocFormat, InitrdProfile, OutputFormat
 from mkosi.documentation import show_docs
-from mkosi.log import log_notice, log_setup
+from mkosi.log import ARG_DEBUG, ARG_DEBUG_SHELL, log_notice, log_setup
 from mkosi.run import find_binary, run, uncaught_exception_handler
 from mkosi.sandbox import __version__, umask
 from mkosi.tree import copy_tree
@@ -298,8 +298,10 @@ def main() -> None:
             ]
 
         if args.debug:
+            ARG_DEBUG.set(args.debug)
             cmdline += ["--debug"]
         if args.debug_shell:
+            ARG_DEBUG_SHELL.set(args.debug_shell)
             cmdline += ["--debug-shell"]
         if args.debug_sandbox:
             cmdline += ["--debug-sandbox"]
