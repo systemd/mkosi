c654d1e76b754d90d881f77fb300a628fe8ff5b8
DaanDeMeyer <daan.j.demeyer@gmail.com>
2025-07-07 15:21:39 +0200

    fedora: Various ELN fixes
    The logic for ELN was quite broken in various places, let's fix it
    up so it actually builds properly.

--- mkosi/distributions/fedora.py
+++ mkosi/distributions/fedora.py
@@ -22,12 +22,11 @@ from mkosi.util import startswith, tuplify
 
 @tuplify
 def find_fedora_rpm_gpgkeys(context: Context) -> Iterable[str]:
-    key1 = find_rpm_gpgkey(
-        context, key=f"RPM-GPG-KEY-fedora-{context.config.release}-primary", required=False
-    )
-    key2 = find_rpm_gpgkey(
-        context, key=f"RPM-GPG-KEY-fedora-{context.config.release}-secondary", required=False
-    )
+    # ELN uses the rawhide GPG keys.
+    release = "rawhide" if context.config.release == "eln" else context.config.release
+
+    key1 = find_rpm_gpgkey(context, key=f"RPM-GPG-KEY-fedora-{release}-primary", required=False)
+    key2 = find_rpm_gpgkey(context, key=f"RPM-GPG-KEY-fedora-{release}-secondary", required=False)
 
     versionre = re.compile(r"RPM-GPG-KEY-fedora-(\d+)-(primary|secondary)")
 
@@ -35,7 +34,7 @@ def find_fedora_rpm_gpgkeys(context: Context) -> Iterable[str]:
         # During branching, there is always a kerfuffle with the key transition.
         # For Rawhide, try to load the N+1 key, just in case our local configuration
         # still indicates that Rawhide==N, but really Rawhide==N+1.
-        if context.config.release == "rawhide" and (rhs := startswith(key1, "file://")):
+        if release == "rawhide" and (rhs := startswith(key1, "file://")):
             if m := versionre.match(Path(rhs).name):
                 version = int(m.group(1))
                 if key3 := find_rpm_gpgkey(
@@ -57,7 +56,7 @@ def find_fedora_rpm_gpgkeys(context: Context) -> Iterable[str]:
                 hint="Make sure the distribution-gpg-keys package is installed",
             )
 
-        if context.config.release == "rawhide":
+        if release == "rawhide":
             # https://fedoraproject.org/fedora.gpg is always outdated when the rawhide key changes. Instead,
             # let's fetch it from distribution-gpg-keys on github, which is generally up-to-date.
             keys = "https://raw.githubusercontent.com/rpm-software-management/distribution-gpg-keys/main/keys/fedora"
@@ -123,7 +122,7 @@ class Installer(DistributionInstaller):
             context,
             list(cls.repositories(context)),
             filelists=False,
-            metadata_expire="6h" if context.config.release == "rawhide" else None,
+            metadata_expire="6h" if context.config.release in ("eln", "rawhide") else None,
         )
 
     @classmethod
@@ -139,15 +138,15 @@ class Installer(DistributionInstaller):
             return
 
         if context.config.release == "eln":
-            mirror = (
-                context.config.mirror
-                or "https://odcs.fedoraproject.org/composes/production/latest-Fedora-ELN/compose"
-            )
-            for repo in ("Appstream", "BaseOS", "Extras", "CRB"):
+            mirror = context.config.mirror or "https://dl.fedoraproject.org/pub/eln/1/"
+
+            for repo in ("AppStream", "BaseOS", "Extras", "CRB"):
                 url = f"baseurl={join_mirror(mirror, repo)}"
                 yield RpmRepository(repo.lower(), f"{url}/$basearch/os", gpgurls)
-                yield RpmRepository(repo.lower(), f"{url}/$basearch/debug/tree", gpgurls, enabled=False)
-                yield RpmRepository(repo.lower(), f"{url}/source/tree", gpgurls, enabled=False)
+                yield RpmRepository(
+                    f"{repo.lower()}-debuginfo", f"{url}/$basearch/debug/tree", gpgurls, enabled=False
+                )
+                yield RpmRepository(f"{repo.lower()}-source", f"{url}/source/tree", gpgurls, enabled=False)
         elif m := context.config.mirror:
             directory = "development" if context.config.release == "rawhide" else "releases"
             url = f"baseurl={join_mirror(m, f'linux/{directory}/$releasever/Everything')}"
