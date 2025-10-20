systemd/mkosi

commit 71ffced0f1796d12d8eb927761646a810ef882f1

Rework PACKAGE_GLOBS to be a PackageManager classmethod

mkosi/installer/pacman.py
@@ -39,6 +39,10 @@ class Pacman(PackageManager):
         return [(Path("pkg"), Path("pkg"))]
 
     @classmethod
+    def package_globs(cls) -> list[str]:
+        return ["*.pkg.tar"]
+
+    @classmethod
     def state_subdirs(cls, state: Path) -> list[Path]:
         return [state / "sync"]
 

mkosi/__init__.py
@@ -48,7 +48,6 @@ from mkosi.bootloader import (
 from mkosi.burn import run_burn
 from mkosi.completion import print_completion
 from mkosi.config import (
-    PACKAGE_GLOBS,
     Args,
     ArtifactOutput,
     Cacheonly,
@@ -1263,7 +1262,10 @@ def install_package_directories(context: Context, directories: Sequence[Path]) -
 
     with complete_step("Copying in extra packages…"):
         for d in directories:
-            for p in itertools.chain(*(d.glob(glob) for glob in PACKAGE_GLOBS)):
+            for p in itertools.chain.from_iterable(
+                d.glob(glob)
+                for glob in context.config.distribution.package_manager(context.config).package_globs()
+            ):
                 shutil.copy(p, context.repository, follow_symlinks=True)
 
 

mkosi/config.py
@@ -1882,15 +1882,6 @@ class Args:
         return dataclasses.replace(cls.default(), **{k: v for k, v in j.items() if k in cls.fields()})
 
 
-PACKAGE_GLOBS = (
-    "*.rpm",
-    "*.pkg.tar*",
-    "*.deb",
-    "*.ddeb",
-    "*.apk",
-)
-
-
 @dataclasses.dataclass(frozen=True)
 class UKIProfile:
     profile: dict[str, str]
@@ -2375,7 +2366,9 @@ class Config:
             "package_directories": [
                 (p.name, p.stat().st_mtime_ns)
                 for d in self.package_directories
-                for p in sorted(flatten(d.glob(glob) for glob in PACKAGE_GLOBS))
+                for p in sorted(
+                    flatten(d.glob(glob) for glob in self.distribution.package_manager(self).package_globs())
+                )
             ],
             "repositories": sorted(self.repositories),
             "overlay": self.overlay,

mkosi/installer/__init__.py
@@ -26,6 +26,10 @@ class PackageManager:
         return []
 
     @classmethod
+    def package_globs(cls) -> list[str]:
+        return []
+
+    @classmethod
     def state_subdirs(cls, state: Path) -> list[Path]:
         return []
 

mkosi/installer/apk.py
@@ -32,6 +32,10 @@ class Apk(PackageManager):
         return [(Path("."), Path("."))]
 
     @classmethod
+    def package_globs(cls) -> list[str]:
+        return ["*.apk"]
+
+    @classmethod
     def scripts(cls, context: Context) -> dict[str, list[PathString]]:
         return {
             "apk": cls.apivfs_script_cmd(context) + cls.env_cmd(context) + cls.cmd(context),

mkosi/installer/apt.py
@@ -6,7 +6,7 @@ from collections.abc import Sequence
 from pathlib import Path
 from typing import Final, Optional
 
-from mkosi.config import PACKAGE_GLOBS, Config, ConfigFeature
+from mkosi.config import Config, ConfigFeature
 from mkosi.context import Context
 from mkosi.installer import PackageManager
 from mkosi.log import die
@@ -58,6 +58,10 @@ class Apt(PackageManager):
     @classmethod
     def package_subdirs(cls, cache: Path) -> list[tuple[Path, Path]]:
         return [(Path("archives"), Path("archives"))]
+
+    @classmethod
+    def package_globs(cls) -> list[str]:
+        return ["*.deb", "*.ddeb"]
 
     @classmethod
     def state_subdirs(cls, state: Path) -> list[Path]:
@@ -286,7 +290,7 @@ class Apt(PackageManager):
 
     @classmethod
     def createrepo(cls, context: Context) -> None:
-        names = [d.name for glob in PACKAGE_GLOBS for d in context.repository.glob(glob) if "deb" in glob]
+        names = [d.name for glob in cls.package_globs() for d in context.repository.glob(glob)]
         if not names:
             return
 

mkosi/installer/dnf.py
@@ -43,6 +43,10 @@ class Dnf(PackageManager):
         ]
 
     @classmethod
+    def package_globs(cls) -> list[str]:
+        return ["*.rpm"]
+
+    @classmethod
     def scripts(cls, context: Context) -> dict[str, list[PathString]]:
         return {
             "dnf": cls.apivfs_script_cmd(context) + cls.env_cmd(context) + cls.cmd(context),

mkosi/installer/zypper.py
@@ -27,6 +27,10 @@ class Zypper(PackageManager):
         return [(Path("packages"), Path("packages"))]
 
     @classmethod
+    def package_globs(cls) -> list[str]:
+        return ["*.rpm"]
+
+    @classmethod
     def scripts(cls, context: Context) -> dict[str, list[PathString]]:
         install: list[PathString] = [
             "zypper",

commit b467d7a4edf9e2701f472cb9f738340e08ec49e0

Match compressed pacman packages too
Arch packages can be compressed using various codecs, in which case the
package filename is suffixed with the compression method (e.g. .zst for
zstandard compression). Make sure to find such compressed packages in
the volatile package directory exposed to the build script as
$PACKAGEDIR, so they are installable using VolatilePackages=.

Fixes: 71ffced0 ("Rework PACKAGE_GLOBS to be a PackageManager classmethod")

mkosi/installer/pacman.py
@@ -40,7 +40,7 @@ class Pacman(PackageManager):
 
     @classmethod
     def package_globs(cls) -> list[str]:
-        return ["*.pkg.tar"]
+        return ["*.pkg.tar*"]
 
     @classmethod
     def state_subdirs(cls, state: Path) -> list[Path]: