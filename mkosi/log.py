systemd/mkosi

commit f26cb34155574966bc053087a2fc68034b40663c

log: set terminal window title in complete_step while mkosi runs

mkosi/log.py
@@ -8,7 +8,7 @@ import sys
 from collections.abc import Iterator
 from typing import Any, NoReturn, Optional
 
-from mkosi.sandbox import Style
+from mkosi.sandbox import Style, terminal_is_dumb
 
 # This global should be initialized after parsing arguments
 ARG_DEBUG = contextvars.ContextVar("debug", default=False)
@@ -32,8 +32,12 @@ def log_step(text: str) -> None:
         # De-emphasize this step here, so the user can tell more
         # easily which step generated the exception. The exception
         # or error will only be printed after we finish cleanup.
+        if not terminal_is_dumb():
+            print(f"\033]0;mkosi: {text}", file=sys.stderr)
         logging.info(f"{prefix}({text})")
     else:
+        if not terminal_is_dumb():
+            print(f"\033]0;mkosi: {text}", file=sys.stderr)
         logging.info(f"{prefix}{Style.bold}{text}{Style.reset}")
 
 
@@ -85,3 +89,17 @@ def log_setup(default_log_level: str = "info") -> None:
     logging.getLogger().setLevel(
         logging.getLevelName(os.getenv("SYSTEMD_LOG_LEVEL", default_log_level).upper())
     )
+
+
+@contextlib.contextmanager
+def stash_terminal_title() -> Iterator[None]:
+    try:
+        # push terminal window title to stack
+        if not terminal_is_dumb():
+            print("\033[22t", file=sys.stderr)
+
+        yield
+    finally:
+        # pop terminal window title from stack to reset
+        if not terminal_is_dumb():
+            print("\033[23t", file=sys.stderr)

mkosi/__main__.py
@@ -10,7 +10,7 @@ from typing import Optional
 import mkosi.resources
 from mkosi import run_verb
 from mkosi.config import parse_config
-from mkosi.log import log_setup
+from mkosi.log import log_setup, stash_terminal_title
 from mkosi.run import find_binary, run, uncaught_exception_handler
 from mkosi.util import resource_path
 
@@ -34,7 +34,7 @@ def main() -> None:
 
     log_setup()
 
-    with resource_path(mkosi.resources) as resources:
+    with resource_path(mkosi.resources) as resources, stash_terminal_title():
         args, tools, images = parse_config(sys.argv[1:], resources=resources)
 
         if args.debug:

commit c5b0792dbbe8d8b8fd5b5529887f5e16da33d304

log: make OSC escape sequences valid
OSC escape sequences ("ESC ]" aka "\033]") need to be terminated with a string
terminator code ST ("\033\").

Commit f26cb341 introduced some OSC escape sequences that resulted in all mkosi
output being hidden when using the Kitty terminal:
https://github.com/kovidgoyal/kitty/issues/9139

Fixes: f26cb341

Co-Authored-By: j <j@cgl.sh>

mkosi/log.py
@@ -33,11 +33,14 @@ def log_step(text: str) -> None:
         # easily which step generated the exception. The exception
         # or error will only be printed after we finish cleanup.
         if not terminal_is_dumb():
-            print(f"\033]0;mkosi: {text}", file=sys.stderr, end="")
+            print(f"\033]0;mkosi: {text}\033\\", file=sys.stderr, end="")
+            # konsole does not support ESC ]0; title ST, but instead supports this
+            print(f"\033]30;mkosi: {text}\033\\", file=sys.stderr, end="")
         logging.info(f"{prefix}({text})")
     else:
         if not terminal_is_dumb():
-            print(f"\033]0;mkosi: {text}", file=sys.stderr, end="")
+            print(f"\033]0;mkosi: {text}\033\\", file=sys.stderr, end="")
+            print(f"\033]30;mkosi: {text}\033\\", file=sys.stderr, end="")
         logging.info(f"{prefix}{Style.bold}{text}{Style.reset}")
 
 