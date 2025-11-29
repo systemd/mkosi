0a0459cca1898b3e8aecba58db20bb619eca086e
Laurence Kiln <246209442+LaurenceKiln@users.noreply.github.com>
2025-11-29 15:45:51 +0200

    Just loop

--- tools/make-man-page.sh
+++ tools/make-man-page.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # SPDX-License-Identifier: LGPL-2.1-or-later
-# shellcheck disable=SC2086 # Allow word splitting on bare vars
 set -ex
 
-PANDOC_ARGS="--lua-filter=mkosi/resources/pandoc/md2man.lua -t man -s"
-pandoc $PANDOC_ARGS -o mkosi/resources/man/mkosi.1 mkosi/resources/man/mkosi.1.md
-pandoc $PANDOC_ARGS -o mkosi/resources/man/mkosi-addon.1 mkosi/resources/man/mkosi-addon.1.md
-pandoc $PANDOC_ARGS -o mkosi/resources/man/mkosi-initrd.1 mkosi/resources/man/mkosi-initrd.1.md
-pandoc $PANDOC_ARGS -o mkosi/resources/man/mkosi-sandbox.1 mkosi/resources/man/mkosi-sandbox.1.md
-pandoc $PANDOC_ARGS -o mkosi/resources/man/mkosi.news.7 mkosi/resources/man/mkosi.news.7.md
+MD_DIR=mkosi/resources/man
+OUTPUT_DIR=mkosi/resources/man
+
+for mdfile in "$MD_DIR"/*.?.md; do
+    pandoc \
+    --lua-filter=mkosi/resources/pandoc/md2man.lua \
+    -s -t man \
+    -o  "${OUTPUT_DIR}/$(basename "${mdfile}" .md)" \
+    "${mdfile}"
+done
