54bbe87efb0d0828e584daa90c5d65c0813a7e1e
Kai Lüke <kai@amutable.com>
2026-03-02 11:39:24 +0900

    Allow printing of step durations with CLI flag
    The printing of the duration for each step was behind the --debug flag
    which adds more noise when one is just interested in the build times.
    Also, the printing of the step duration serves as good marker during
    log reading to understand where the above log output belongs to.
    Add a new --log-timing flag to always print the step durations.

--- mkosi/config.py
+++ mkosi/config.py
@@ -33,7 +33,7 @@ from pathlib import Path
 from typing import Any, Callable, ClassVar, Generic, Optional, Protocol, TypeVar, Union, cast
 
 from mkosi.distribution import Distribution, detect_distribution
-from mkosi.log import ARG_DEBUG, ARG_DEBUG_SANDBOX, ARG_DEBUG_SHELL, complete_step, die
+from mkosi.log import ARG_DEBUG, ARG_DEBUG_SANDBOX, ARG_DEBUG_SHELL, ARG_LOG_TIMING, complete_step, die
 from mkosi.pager import page
 from mkosi.run import SandboxProtocol, find_binary, nosandbox, run, sandbox_cmd, workdir
 from mkosi.sandbox import ANSI_BLUE, ANSI_BOLD, ANSI_RESET, __version__
@@ -1871,6 +1871,7 @@ class Args:
     debug_shell: bool
     debug_workspace: bool
     debug_sandbox: bool
+    log_timing: bool
     pager: bool
     genkey_valid_days: str
     genkey_common_name: str
@@ -4535,6 +4536,12 @@ def create_argument_parser(chdir: bool = True) -> argparse.ArgumentParser:
         default=False,
     )
     parser.add_argument(
+        "--log-timing",
+        help="Log the duration of each build step",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
         "--no-pager",
         action="store_false",
         dest="pager",
@@ -5362,6 +5369,8 @@ def parse_config(
         ARG_DEBUG_SHELL.set(args.debug_shell)
     if args.debug_sandbox:
         ARG_DEBUG_SANDBOX.set(args.debug_sandbox)
+    if args.log_timing:
+        ARG_LOG_TIMING.set(args.log_timing)
 
     if args.rerun_build_scripts and not args.verb.needs_build():
         die(f"--rerun-build-scripts cannot be used with the '{args.verb}' command")
